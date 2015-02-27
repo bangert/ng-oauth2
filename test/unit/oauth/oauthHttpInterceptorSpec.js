77e9d2e120157638437837748e7d4e297c385f35
David Ahern <david.ahern@emc.com>
2015-02-27 14:37:06 +0000

    Testing failed test on travis

--- test/unit/oauth/oauthHttpInterceptorSpec.js
+++ test/unit/oauth/oauthHttpInterceptorSpec.js
@@ -28,7 +28,7 @@ describe('OauthHttpInterceptor', function () {
             Oauth.sendTokenOnEveryRequest = true;
             OauthProfile.makeProfileRequest();
             $httpBackend.flush();
-            expect(token).toBe('test_token');
+            expect(token).toBe('test_token1');
         }));
 
         it('should not add the authorization token to the request when sendTokenOnEveryRequest is disabled', inject(function (Oauth, OauthProfile) {

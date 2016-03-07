43af4f86e7c5b6b6a6bbb88a19c06f0aa200c22c
David Ahern <david.ahern@emc.com>
2016-03-07 16:31:39 +0000

    Bumped version and built

--- dist/ng-oauth2.js
+++ dist/ng-oauth2.js
@@ -100,12 +100,16 @@ angular.module('ng-oauth2.endpoint', []).factory('OauthEndpoint', ['$window', '$
     };
 
 }]);
-angular.module('ng-oauth2').factory('OauthHttpInterceptor', ['$q', 'Oauth', 'OauthToken', function ($q, Oauth, OauthToken) {
+angular.module('ng-oauth2').factory('OauthHttpInterceptor', ['Oauth', 'OauthToken', function (Oauth, OauthToken) {
     return {
         request: function (config) {
 
             if (Oauth.sendTokenOnEveryRequest && OauthToken.getToken()) {
-                config.headers[Oauth.headerTokenName] = OauthToken.getToken().access_token;
+                if (Oauth.headerTokenName.toLowerCase == 'authorization') {
+                    config.headers[Oauth.headerTokenName] = 'Bearer ' + OauthToken.getToken().access_token;
+                } else {
+                    config.headers[Oauth.headerTokenName] = OauthToken.getToken().access_token;
+                }
             }
 
             return config;
